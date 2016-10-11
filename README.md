21ec44936fa22d218dbc68de14f796af5abeedb4
Aneesh Ashutosh <aashutosh@twitter.com>
2016-10-11 14:59:44 -0400

    added json generator

--- README.md
+++ README.md
@@ -4,8 +4,18 @@
 
 I developed this script to automate the sending of emails for Tech@NYU's business development team. Previously, we would have to manually send emails to everyone we wished to reach out to. This is not sufficient in our modern time when human labor is so valuable and we have the opportunity to replace it completely with robots.
 
+# Installation
+
+`git clone https://github.com/TechAtNYU/BD-email-bot.git`
+`cd BD-email-bot`
+
+
 # Example Usage
 
+**To create a json object and save it into example.json**
+`python generate-json.py example.json`
+
+**To execute the bot from the contents of example.json**
 `python mailbot.py example.json`
 
 # To-do
--- mailbot.py
+++ mailbot.py
@@ -4,37 +4,40 @@ import sys
 from email.mime.text import MIMEText as text
 
 if len(sys.argv) != 2:
-	print("Incorrect number of arguments.")
-	sys.exit(0)
+    print("Incorrect number of arguments.")
+    sys.exit(0)
 
-with open(sys.argv[1]) as data_file:    
+with open(sys.argv[1]) as data_file:
     data = json.load(data_file)
 
-# configure server
-server = smtplib.SMTP('smtp.gmail.com:587') # try to see if there's a way to send from NYU servers
-variable_dictionary = {}
-
 # populate data fields
-from_address = data["from"]
+from_address = data["name"]
 username = data["username"]
 password = data["password"]
 raw_message = data["message"]
 raw_subject = data["subject"]
 variable_to = data["to"]
+variable_dictionary = {}
+
+# configure server
+# try to see if there's a way to send from NYU servers
+server = smtplib.SMTP('smtp.gmail.com:587')
+if "@nyu.edu" in username:
+    server = smtplib.SMTP('smtp.nyu.com:465')
 
 # populate the dictionary of variables and replacers
 list_length = -1
 for rows in data["variables"]:
-	var = rows["var"]
-	list_vals = []
-	for vals in rows["vals"]:
-		list_vals.append(vals["val"])
-	if (list_length == -1):
-		list_length = len(list_vals)
-	elif list_length != len(list_vals):
-		print("Error: every list of variables must be of the same length.")
-		sys.exit(0)
-	variable_dictionary[var] = list_vals
+    var = rows["var"]
+    list_vals = []
+    for vals in rows["vals"]:
+        list_vals.append(vals["val"])
+    if (list_length == -1):
+        list_length = len(list_vals)
+    elif list_length != len(list_vals):
+        print("Error: every list of variables must be of the same length.")
+        sys.exit(0)
+    variable_dictionary[var] = list_vals
 
 server.ehlo()
 server.starttls()
@@ -42,21 +45,21 @@ server.login(username, password)
 
 # replace variables in the message
 for i in range(0, list_length):
-	message = raw_message
-	subject = raw_subject
-	to_address = variable_dictionary[variable_to][i]
-	sys.stdout.write("Sending email to " + to_address + "... ")
-	for key, values in variable_dictionary.iteritems():
-		message = message.replace(key, values[i])
-		subject = subject.replace(key, values[i])
+    message = raw_message
+    subject = raw_subject
+    to_address = variable_dictionary[variable_to][i]
+    sys.stdout.write("Sending email to " + to_address + "... ")
+    for key, values in variable_dictionary.iteritems():
+        message = message.replace(key, values[i])
+        subject = subject.replace(key, values[i])
 
-	m = text(message)
-	m['Subject'] = subject
-	m['From'] = from_address
-	m['To'] = to_address
-	
-	# send the message out
-	server.sendmail(from_address, to_address, m.as_string())
-	sys.stdout.write("Done!\n")
+    m = text(message)
+    m['Subject'] = subject
+    m['From'] = from_address
+    m['To'] = to_address
+
+    # send the message out
+    server.sendmail(from_address, to_address, m.as_string())
+    sys.stdout.write("Done!\n")
 
 server.quit()
